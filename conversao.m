4db3b5a426edc47ec4f9f46daf562c16616ab596
Pamelaevangelista <panuemanuelecilva@gmail.com>
2025-02-21 21:35:18 -0300

    19/20/21

---
+++ conversao.m
@@ -0,0 +1,38 @@
+function celsius_fahrenheit()
+
+ while true
+
+   disp('Escolher uma das operações');
+   disp('1: Celsius para fahrenheit');
+   disp('2: Fahrenheit para Celsius');
+   disp('3: Sair');
+
+   escolher = input('Digite uma das opções desejada:' );
+
+   switch escolher
+     case 1
+       celsius = input('Digite a Temperatura em ºC:');
+       resultado = (celsius * 9/5) + 32;
+       fprintf('Resultado: %.2f ºF\n' ,resultado);
+
+      case 2
+        fahrenheit = input('Digite a temperatura em ºF ');
+        resultado = (fahrenheit - 32)*5/9;
+        fprintf('Resultado: %.2f ºC\n' ,resultado);
+
+      case 3
+        disp ('Encerrado o programa.');
+        return;
+       otherwise
+        disp('Opção inválida. Por favor, escolha uma operação válida.');
+      end
+
+      continuar = input('Deseja continuar? (S/N)', 's')
+      if lower(continuar) ~= 's'
+        disp('Encerrado o programa!')
+        break;
+      endif
+   end
+
+ endfunction
+
---
+++ distancia.m
@@ -0,0 +1,24 @@
+while true
+  velocidade = input('Digite a velocidade inicial do carro ( em km/h) (ou "s" para sair do programa): ', 's');
+
+  if strcmp(velocidade, 's')
+    disp('Encerrado o programa')
+    break;
+  endif
+
+  aceleracao = input('Digiye a taxa de desaceleração (em m/s²): ');
+  velocidade = str2double(velocidade);
+
+  if isnan(velocidade) || velocidade<=0 || isnan(aceleracao) || aceleracao <=0
+    disp('Por favor , insira valores válidos');
+    continue;
+  endif
+
+  #converto  a velocidade em km/h para m/s
+  velocidade_mps = velocidade / 3.6;
+
+  #calculo a distancia
+  distancia = (velocidade_mps^2) / ( 2 * aceleracao);
+
+  fprintf('A distância percorrida até para completamente é: %.2f metros \n' , distancia)
+endwhile
